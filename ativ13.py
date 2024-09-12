0afeaf7570eaf60f73fbffd02d96ea581d29551b
NaterciaMM <naterciamm2020@gmail.com>
2024-09-12 13:16:03 -0300

    atividade13

---
+++ ativ13.py
@@ -0,0 +1,12 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+
+num = float(input("insira um numero: "))
+
+if num <0:
+    print("numereo negativo")
+
+elif num ==0:
+    print("numero zero")
+
+else:
+    print("numero positivo")
